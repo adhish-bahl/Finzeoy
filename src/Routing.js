104ce9e40af147c6c45a9a5c357c6e58d9a6c904
adhish-bahl <adhishbahl0@gmail.com>
2022-08-27 10:32:15 +0530

    Downgraded React-router-dom

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useHistory, Redirect } from "react-router-dom";
+
 import PageNotFound from './PageNotFound'
 
 function Routing() {
