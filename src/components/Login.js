e11bf6fd89efe389876f08065a3ba70d728f0b28
Kavish Bamboli <bambolikavish20@gmail.com>
2022-09-07 23:45:21 +0530

    Wired state to login and sign up componenets

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/SignUpStyles.css'
 
 export default function Login() {
+
+    const [loginDetails, setLoginDetails] = useState({
+        email: "",
+        password: "",
+        userType: "end-user"
+    })
+
+    function handleChange(event) {
+        // validation code        
+        const {name, value} = event.target;
+        setLoginDetails(prevFormData => {
+            return {
+                ...prevFormData,
+                [name] : value
+            }
+        })
+    }
+
+    function loginUser() {
+        // Code to authenticate user and display pages based on user type
+    }
+
+    return (
+        <div className="login--container">
+            <div>
+                <h1 className="signUp--head">LOG IN</h1>
+                <h5 className="signUp--head">STAY UPDATED ON THE FINANCIAL WORLD</h5>
+            </div>
+            <form className="signUp--form">
+                <input type={"email"} placeholder="Email ID" className="form--input" name="email" onChange={handleChange}></input>
+                <input type={"password"} placeholder="Password" className="form--input" name="password" onChange={handleChange}></input>
+                <fieldset className="form--radioInput">
+                    <legend className="form--radioInput--legend">User Type</legend>
+                    <input type={"radio"} name="userType" id="endUser" value="end-user" checked={loginDetails.userType === "end-user"} onChange={handleChange}></input>
+                    <label htmlFor="endUser">End User</label>
+                    <input type={"radio"} name="userType" id="advisor" value="financial-advisor" checked={loginDetails.userType === "financial-advisor"} onChange={handleChange}></input>
+                    <label htmlFor="advisor">Advisor</label>
+                    <input type={"radio"} name="userType" id="student" value="student" checked={loginDetails.userType === "student"} onChange={handleChange}></input>
+                    <label htmlFor="student">Student</label>
+                </fieldset>
+                <a href="#" className="form--forgotPass">Forgot your Password?</a>
+                <button className="form--submit" onClick={loginUser}>Login</button>
+            </form>
+        </div>
+    )
     
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/SignUpStyles.css'
 
 export default function SignUp() {
+
+    const [userDetails, setUserDetails] = useState({
+        name: "",
+        email: "",
+        phno: "",
+        userType: "none",
+        password: ""
+    });
+
+    function handleChange(event) {
+        // validation code
+        const {name, value} = event.target;
+        setUserDetails(prevFormData => {
+            return {
+                ...prevFormData,
+                [name] : value
+            }
+        })
+    }
+    
+    function saveFormData() {
+        //Code to call backend and save form data
+    }
+
     return (
         <div className="signUp--container">
             <div>
@@ -9,17 +33,17 @@ export default function SignUp() {
                 <h5 className="signUp--head">STAY UPDATED ON THE FINANCIAL WORLD</h5>
             </div>
             <form className="signUp--form">
-                <input type={"text"} placeholder="Name" className="form--input"></input>
-                <input type={"email"} placeholder="Email ID" className="form--input"></input>
-                <input type={"tel"} placeholder="Phone Number" className="form--input"></input>
-                <select className="form--input">
-                    <option value="none" selected disabled hidden>User Type</option>
-                    <option>End User</option>
-                    <option>Financial Advisor</option>
-                    <option>Student</option>
+                <input type={"text"} placeholder="Name" className="form--input" name="name" value={userDetails.name} onChange={handleChange}></input>
+                <input type={"email"} placeholder="Email ID" className="form--input" name="email" value={userDetails.email} onChange={handleChange}></input>
+                <input type={"tel"} placeholder="Phone Number" className="form--input" name="phno" value={userDetails.phno} onChange={handleChange}></input>
+                <select name="userType" value={userDetails.userType} className="form--input" onChange={handleChange}>
+                    <option value="none" hidden>User Type</option>
+                    <option value="end-user">End User</option>
+                    <option value="financial-advisor">Financial Advisor</option>
+                    <option value="student">Student</option>
                 </select>
-                <input type={"password"} placeholder="Password" className="form--input"></input>
-                <button className="form--submit">Join Now</button>
+                <input type={"password"} placeholder="Password" className="form--input" name="password" value={userDetails.password} onChange={handleChange}></input>
+                <button className="form--submit" onClick={saveFormData}>Join Now</button>
             </form>
         </div>
     )
