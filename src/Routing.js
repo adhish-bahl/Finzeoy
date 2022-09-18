88719fffbde0d928ef3766bc7dd4318d937d246a
adhish-bahl <adhishbahl0@gmail.com>
2022-09-18 19:26:45 +0530

    Landing Page done

--- src/Routing.js
+++ src/Routing.js
@@ -16,6 +16,7 @@ function Routing() {
                 <Switch>
 
                     <Route exact path="/">
+                        <LoginSignUpHeader />
                         <LandingPage />
                     </Route>
                     
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,17 +2,27 @@ import React from 'react'
 import LPBG from "../Images/LandingPageBG.png"
 import icon from "../Images/Ambassador-bro 1.svg"
 import "../styles/LandingPage.css"
+import wallet from "../Images/Wallet.svg"
+import coinStack from "../Images/CoinStack.svg"
 
 function LandingPage() {
   return (
     <div className='LandingPageContainer'>
         <div className="LPleft">
-          <img src={LPBG} alt="Background" />
+          <img className='backgrounfLeft' src={LPBG} alt="Background" />
         </div>
 
-        <img src={icon} alt="man" />
+        <img className='manIcon'  src={icon} alt="man" />
 
         <div className="LPright">
+          <div className="LPrightTop">
+            <img src={wallet} className="LPWallet" alt="Wallet" />
+            Finzeoy is a personal finance management system. Finance, of financing, is the process of raising funds or capital for any kind of expenditure. It is the process of channeling various funds in the form of credit, loans, or invested capital to those economic entities that most need them or can put them to the most productive use.
+            <img className='LPCoinStack' src={coinStack} alt="Coins" />
+          </div>
+          <div className="LPrightLower">
+          © FINZEOY 2022
+          </div>
         </div>
     </div>
   )
