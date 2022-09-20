6941d2195fe5f021a47429c4911be9630124a902
adhish-bahl <adhishbahl0@gmail.com>
2022-09-20 15:51:58 +0530

    done for the dayyyyyygit add .!

--- src/components/AdvisorProfile.js
+++ src/components/AdvisorProfile.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import "../styles/AdvisorProfile.css"
+
 
 function AdvisorProfile() {
   return (
-    <div>AdvisorProfile</div>
+    <div className='APcontainer'>
+        <div className="APleft">
+
+            <div className="APLone">
+                <div className="APLOleft">
+                    <img className='APLOLprofileimg' src="./Images/default_logo.png" alt="profileImg" />
+                </div>
+
+                <div className="APLOright">
+                    <div className="APadname">Name</div>
+                    <div className="APadage">Age</div>
+                    <div className="APadprofession">Profession</div>
+                </div>
+            </div>
+
+            <div className="APLtwo">
+                <h1 className="APheading">Profile</h1>
+            </div>
+
+            <div className="APLthree">
+                <h1 className="APheading">Contact</h1>
+                
+                <div className="APLTtags">
+                    <div className="tags">Phone No.</div>
+                    <div className="tags">Twitter</div>
+                    <div className="tags">Email Id</div>
+                    <div className="tags">Facebook</div>
+                    <div className="tags">LinkedIn</div>
+                    <div className="tags">Website</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="APright">
+
+        </div>
+    </div>
   )
 }
 
