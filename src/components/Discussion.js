9525bb8c88d71e7f169e54b3db22c7f4a5ff6f7b
adhish-bahl <adhishbahl0@gmail.com>
2022-10-01 11:48:59 +0530

    worked on discussion forum

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../styles/Discussion.css"
 
 function Discussion() {
   return (
-    <div>Discussion</div>
+    <div className='discussionContainer'>
+        <div className="DCleft">
+            <div className="DClheading">
+                <h1>My Questions</h1>
+            </div>
+    
+            <div className="DCRupper">
+                <h1>Questions Asked:</h1>
+            </div>
+
+            <div className="DCRlower">
+                <h1>Question Answered:</h1>
+            </div>
+        </div>
+
+        <div className="DCright">
+            
+        </div>
+    </div>
   )
 }
 
