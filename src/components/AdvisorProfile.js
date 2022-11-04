2707c0816546f5c7f90781f742d3e5e97b00972a
Priyanshi Mundhra <bambolikavish20@gmail.com>
2022-11-04 12:26:18 +0530

    Connected advisors forum

--- src/components/AdvisorProfile.js
+++ src/components/AdvisorProfile.js
@@ -1,52 +1,78 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import "../styles/AdvisorProfile.css"
 import AdvisorQuestionComp from './AdvisorQuestionComp'
 
 
-function AdvisorProfile() {
-  return (
-    <div className='APcontainer'>
-        <div className="APleft">
+function AdvisorProfile(props) {
 
-            <div className="APLone">
-                <div className="APLOleft">
-                    <img className='APLOLprofileimg' src="./Images/default_logo.png" alt="profileImg" />
+    const [questionsData, setQuestionsData] = React.useState([]);
+    const [advisorInfo, setAdvisorInfo] = React.useState({});
+
+    useEffect(() => {
+        fetch("https://finzeoy.000webhostapp.com/GetQuestionsData.php")
+        .then(res => res.json())
+        .then(data => setQuestionsData(data))
+
+        fetch("https://finzeoy.000webhostapp.com/GetAdvisorInfo.php?userId="+props.userId+"")
+        .then(res => res.json())
+        .then(data => setAdvisorInfo(prevState => {
+            return {...prevState,
+                    "name": data.name,
+                    "email": data.email,
+                    "phno": data.phno}
+        }))
+
+        fetch("https://finzeoy.000webhostapp.com/GetUserData.php?userId="+props.userId+"")
+    }, [])
+
+    const unansweredQuestionsData = questionsData.filter(question => {
+        return question.answer.length === 0
+    })
+
+    const unansweredQuestions = unansweredQuestionsData.map(question => {
+        return <AdvisorQuestionComp question={question.ques} author={question.quesBy} />
+    })
+
+    return (
+        <div className='APcontainer'>
+            <div className="APleft">
+
+                <div className="APLone">
+                    <div className="APLOleft">
+                        <img className='APLOLprofileimg' src="./Images/default_logo.png" alt="profileImg" />
+                    </div>
+
+                    <div className="APLOright">
+                        <div className="APadname">{advisorInfo.name}</div>
+                        <div className="APadage">{advisorInfo.age}</div>
+                        <div className="APadprofession">{advisorInfo.profession}</div>
+                    </div>
                 </div>
 
-                <div className="APLOright">
-                    <div className="APadname">Name</div>
-                    <div className="APadage">Age</div>
-                    <div className="APadprofession">Profession</div>
+                <div className="APLtwo">
+                    <h1 className="APheading">{advisorInfo.desc}</h1>
                 </div>
-            </div>
 
-            <div className="APLtwo">
-                <h1 className="APheading">Profile</h1>
-            </div>
-
-            <div className="APLthree">
-                <h1 className="APheading">Contact</h1>
-                
-                <div className="APLTtags">
-                    <div className="tags">Phone No.</div>
-                    <div className="tags">Twitter</div>
-                    <div className="tags">Email Id</div>
-                    <div className="tags">Facebook</div>
-                    <div className="tags">LinkedIn</div>
-                    <div className="tags">Website</div>
+                <div className="APLthree">
+                    <h1 className="APheading">Contact</h1>
+                    
+                    <div className="APLTtags">
+                        <div className="tags">{advisorInfo.phno}</div>
+                        <div className="tags">{advisorInfo.twitter}</div>
+                        <div className="tags">{advisorInfo.email}</div>
+                        <div className="tags">{advisorInfo.facebook}</div>
+                        <div className="tags">{advisorInfo.linkedin}</div>
+                        <div className="tags">{advisorInfo.website}</div>
+                    </div>
                 </div>
+
             </div>
 
+            <div className="APright">
+                {unansweredQuestions}
+            </div>
         </div>
-
-        <div className="APright">
-            <AdvisorQuestionComp question="How can my income Expenditure improve?" author="By althea D’Cruz" />
-            <AdvisorQuestionComp question="How can my income Expenditure improve?" author="By althea D’Cruz" />
-            <AdvisorQuestionComp question="How can my income Expenditure improve?" author="By althea D’Cruz" />
-            <AdvisorQuestionComp question="How can my income Expenditure improve?" author="By althea D’Cruz" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default AdvisorProfile
