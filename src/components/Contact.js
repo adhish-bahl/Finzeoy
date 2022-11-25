0c0d44ecd1cd3c05ff80116a674cb0c0e9f31790
adhish-bahl <adhishbahl0@gmail.com>
2022-11-26 03:20:57 +0530

    isse=ue of social media handles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,11 +17,11 @@ export default function Contact() {
                         <div className="name">Adhish Bahl</div>
                         <div className="intro">Heya! I am a self taught web developer and I am more interested in Front-end development. My experties are HTML V5, CSS V3, JavaScript along with ReactJS for front end development. Other then web development, Video Editing and Photography also interests me. Also I am coffee addict :)
                             <div className="socialMediaCreater">
-                                <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
-                                    <img src={linkedin} alt="linkedIn" target="_blank" style={{width: "30px"}} />
+                                <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/" target="_blank">
+                                    <img src={linkedin} alt="linkedIn" style={{ width: "30px" }} />
                                 </a>
-                                <a href="https://github.com/adhish-bahl">
-                                    <img src={github} alt="linkedIn" target="_blank" style={{width: "30px"}} />
+                                <a href="https://github.com/adhish-bahl" target="_blank">
+                                    <img src={github} alt="linkedIn" style={{ width: "30px" }} />
                                 </a>
                             </div>
                         </div>
@@ -31,11 +31,11 @@ export default function Contact() {
                         <div className="intro">
                             Hi, I am Gayathri. I’d describe myself as a person largely driven by curiosity. Being a student at Christ (Deemed to be University) has provided me the opportunity to come up with this website and provided me an outlet where I along with my team have given direction to our curiosity and shaped our ideas to life. I’ve always envisioned on creating a platform that brings together like minded people and T-Une is just that!
                             <div className="socialMediaCreater">
-                                <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
-                                    <img src={linkedin} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                                <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/" target="_blank">
+                                    <img src={linkedin} alt="linkedIn" style={{ width: "30px" }} />
                                 </a>
-                                <a href="https://github.com/adhish-bahl">
-                                    <img src={github} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                                <a href="https://github.com/adhish-bahl" target="_blank">
+                                    <img src={github} alt="linkedIn" style={{ width: "30px" }} />
                                 </a>
                             </div>
                         </div>
@@ -44,11 +44,11 @@ export default function Contact() {
                         <div className="name">Priyanshi S. Mundhra</div>
                         <div className="intro">I am a student pursuing BCA in Christ University. My interest lies in back-end development and AI. my expertise are in MySQL, HTML5 and C. Among my other interest is Music and art.</div>
                         <div className="socialMediaCreater">
-                            <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
-                                <img src={linkedin} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                            <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/" target="_blank">
+                                <img src={linkedin} alt="linkedIn" style={{ width: "30px" }} />
                             </a>
-                            <a href="https://github.com/adhish-bahl">
-                                <img src={github} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                            <a href="https://github.com/adhish-bahl" target="_blank">
+                                <img src={github} alt="linkedIn" style={{ width: "30px" }} />
                             </a>
                         </div>
                     </div>
@@ -56,11 +56,11 @@ export default function Contact() {
                         <div className="name">Riddhish Savalkar</div>
                         <div className="intro">I am a student pursuing BCA in Christ University. My interest lies in back-end development and AI. my expertise are in MySQL, HTML5 and C. Among my other interest is Music and art.</div>
                         <div className="socialMediaCreater">
-                            <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/">
-                                <img src={linkedin} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                            <a href="https://www.linkedin.com/in/adhish-bahl-4113591b7/" target="_blank">
+                                <img src={linkedin} alt="linkedIn" style={{ width: "30px" }} />
                             </a>
-                            <a href="https://github.com/adhish-bahl">
-                                <img src={github} alt="linkedIn" target="_blank" style={{ width: "30px" }} />
+                            <a href="https://github.com/adhish-bahl" target="_blank">
+                                <img src={github} alt="linkedIn" style={{ width: "30px" }} />
                             </a>
                         </div>
                     </div>
